74dbd9c619bfe50eb9c8eeaf9b599ee12a3ecc3d
Arxoto <1484663586@qq.com>
2025-11-28 21:02:26 +0800

    在adapter中增加from_str实现

--- src/godot_ext_impl/adapter.rs
+++ src/godot_ext_impl/adapter.rs
@@ -6,6 +6,12 @@ use crate::cores::unify_type::{FixedName, FixedString};
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct FixedNameWrapper(pub StringName);
 
+impl From<&str> for FixedNameWrapper {
+    fn from(value: &str) -> Self {
+        FixedNameWrapper(StringName::from(value))
+    }
+}
+
 impl From<StringName> for FixedNameWrapper {
     fn from(value: StringName) -> Self {
         FixedNameWrapper(value)
@@ -23,6 +29,12 @@ impl FixedName for FixedNameWrapper {}
 /// same as [`FixedString`]
 #[derive(PartialEq, Eq, Hash, Clone, Debug, Default)]
 pub struct FixedStringWrapper(pub GString);
+
+impl From<&str> for FixedStringWrapper {
+    fn from(value: &str) -> Self {
+        FixedStringWrapper(GString::from(value))
+    }
+}
 
 impl From<GString> for FixedStringWrapper {
     fn from(value: GString) -> Self {
